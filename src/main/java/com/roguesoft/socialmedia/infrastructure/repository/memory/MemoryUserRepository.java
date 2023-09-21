777412621e0bbc4237e5737cd6d66d0e6c7e4513
Marcos Campos <contato.marcoscampos@gmail.com>
2023-09-21 20:37:10 -0300

    Inserindo um memory repository para User

---
+++ src/main/java/com/roguesoft/socialmedia/infrastructure/repository/memory/MemoryUserRepository.java
@@ -0,0 +1,31 @@
+package com.roguesoft.socialmedia.infrastructure.repository.memory;
+
+import com.roguesoft.socialmedia.infrastructure.model.UserModel;
+import com.roguesoft.socialmedia.infrastructure.repository.UserRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+@Repository
+public class MemoryUserRepository implements UserRepository {
+
+    private final Map<String, UserModel> db = new HashMap<>();
+
+    @Override
+    public UserModel save(UserModel model) {
+        db.put(model.getId(), model);
+        return model;
+    }
+
+    @Override
+    public Optional<UserModel> findById(String id) {
+        UserModel model = db.get(id);
+        if(Objects.nonNull(model)){
+            return Optional.of(model);
+        }
+        return Optional.empty();
+    }
+}
